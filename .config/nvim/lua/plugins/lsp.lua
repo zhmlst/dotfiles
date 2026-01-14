956f799bed85c53c3f3409de2b67e5afb4c72213
zhmlst <zhmlst.btw@gmail.com>
2026-01-14 23:37:17 +0300

    backup

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -1,9 +1,91 @@
 vim.g.vim_json_warnings = 0
 return {
   {
+    'nvim-treesitter/nvim-treesitter',
+    branch = 'main',
+    dependencies = { 'WhoIsSethDaniel/mason-tool-installer.nvim' },
+    build = ':TSUpdate',
+    config = function()
+      local ts = require('nvim-treesitter')
+
+      ts.setup({
+        install_dir = vim.fn.stdpath('data') .. '/site',
+      })
+
+      local langs = {
+        'lua',
+        'go',
+        'rust',
+        'sql',
+        'yaml',
+        'toml',
+        'bash',
+        'json',
+        'markdown',
+      }
+
+      vim.api.nvim_create_autocmd('User', {
+        pattern = 'MasonToolsUpdateCompleted',
+        callback = function()
+          ts.install(langs)
+        end,
+      })
+
+      vim.api.nvim_create_autocmd('FileType', {
+        callback = function(args)
+          local lang = vim.treesitter.language.get_lang(
+            vim.bo[args.buf].filetype
+          ) or vim.bo[args.buf].filetype
+
+          if lang and vim.treesitter.query.get(lang, 'highlights') then
+            vim.treesitter.start(args.buf, lang)
+            vim.bo[args.buf].indentexpr =
+              "v:lua.require'nvim-treesitter'.indentexpr()"
+          end
+        end,
+      })
+    end,
+  },
+
+  {
+    'saghen/blink.cmp',
+    version = 'v0.*',
+    opts = {
+      keymap = {
+        preset = 'none',
+        ['<C-j>'] = { 'select_and_accept', 'fallback' },
+
+        ['<C-space>'] = { 'show', 'hide', 'fallback' },
+
+        ['<C-n>'] = { 'select_next', 'fallback' },
+        ['<C-p>'] = { 'select_prev', 'fallback' },
+
+        ['<C-u>'] = { 'scroll_documentation_up', 'fallback' },
+        ['<C-d>'] = { 'scroll_documentation_down', 'fallback' },
+      },
+
+      completion = {
+        documentation = {
+          auto_show = true,
+          auto_show_delay_ms = 0,
+        },
+      },
+
+      appearance = {
+        use_nvim_cmp_as_default = true,
+        nerd_font_variant = 'mono',
+      },
+      sources = {
+        default = { 'lsp', 'path', 'snippets', 'buffer' },
+      },
+    },
+  },
+
+  {
     'WhoIsSethDaniel/mason-tool-installer.nvim',
     opts = {
       ensure_installed = {
+        'tree-sitter-cli',
         'lua-language-server',
         'stylua',
         'gopls',
@@ -17,6 +99,8 @@ return {
         'bash-language-server',
         'shfmt',
         'prettier',
+        'rust-analyzer',
+        'rustfmt',
       },
       auto_update = false,
       run_on_start = true,
@@ -25,8 +109,10 @@ return {
 
   {
     'neovim/nvim-lspconfig',
-    dependencies = { 'williamboman/mason.nvim' },
+    dependencies = { 'williamboman/mason.nvim', 'saghen/blink.cmp' },
     config = function()
+      local capabilities = require('blink.cmp').get_lsp_capabilities()
+
       local servers = {
         lua_ls = {
           settings = {
@@ -42,7 +128,9 @@ return {
         yamlls = {},
         taplo = {},
         bashls = {},
+        rust_analyzer = {},
       }
+
       vim.api.nvim_create_autocmd('LspAttach', {
         callback = function(args)
           local opts = { buffer = args.buf, silent = true }
@@ -56,6 +144,7 @@ return {
       })
 
       for name, config in pairs(servers) do
+        config.capabilities = capabilities
         vim.lsp.config(name, config)
         vim.lsp.enable(name)
       end
@@ -76,9 +165,9 @@ return {
         toml = { 'taplo' },
         sh = { 'shfmt' },
         bash = { 'shfmt' },
+        rust = { 'rustfmt' },
       },
       format_on_save = {
-        timeout_ms = 500,
         lsp_fallback = true,
       },
     },
